f77bf6ac3dbad13683f8ec732f79fa651d8d1e31
raj.jadon <raj.jadon@olaelectric.com>
2022-09-10 06:22:22 +0530

    add fragment class

--- prasentation/src/main/java/com/raj/jadon/prasentation/trendingReposiotry/TreadingRepositoryFragment.kt
+++ prasentation/src/main/java/com/raj/jadon/prasentation/trendingReposiotry/TreadingRepositoryFragment.kt
@@ -1,3 +1,20 @@
 package com.raj.jadon.prasentation.trendingReposiotry
 
-class TreadingRepositoryFragment
+import com.raj.jadon.prasentation.R
+import com.raj.jadon.prasentation.common.base.BaseFragment
+import com.raj.jadon.prasentation.databinding.FragmentTrendingRepositoryBinding
+
+class TreadingRepositoryFragment :
+    BaseFragment<FragmentTrendingRepositoryBinding>(R.layout.fragment_trending_repository) {
+    override fun setDataCollector() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setUpBindingVariables() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setClickListener() {
+        TODO("Not yet implemented")
+    }
+}
