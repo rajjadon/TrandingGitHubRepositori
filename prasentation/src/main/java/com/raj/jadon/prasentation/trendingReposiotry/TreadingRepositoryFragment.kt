b590f30e7ef77d28fd3a49b9c401da1c2b88b023
raj.jadon <raj.jadon@olaelectric.com>
2022-09-10 06:24:52 +0530

    inject view model properly

--- prasentation/src/main/java/com/raj/jadon/prasentation/trendingReposiotry/TreadingRepositoryFragment.kt
+++ prasentation/src/main/java/com/raj/jadon/prasentation/trendingReposiotry/TreadingRepositoryFragment.kt
@@ -3,9 +3,15 @@ package com.raj.jadon.prasentation.trendingReposiotry
 import com.raj.jadon.prasentation.R
 import com.raj.jadon.prasentation.common.base.BaseFragment
 import com.raj.jadon.prasentation.databinding.FragmentTrendingRepositoryBinding
+import com.raj.jadon.prasentation.trendingReposiotry.viewModel.TreadingRepositoryViewModel
+import javax.inject.Inject
 
 class TreadingRepositoryFragment :
     BaseFragment<FragmentTrendingRepositoryBinding>(R.layout.fragment_trending_repository) {
+
+    @Inject
+    lateinit var trendingViewModel: TreadingRepositoryViewModel
+
     override fun setDataCollector() {
         TODO("Not yet implemented")
     }
