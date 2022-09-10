7944f49e3791b3e4beafeb32de2ddff7f9ca136f
raj.jadon <raj.jadon@olaelectric.com>
2022-09-10 06:45:43 +0530

    inject view model properly

--- prasentation/src/main/java/com/raj/jadon/prasentation/trendingReposiotry/TreadingRepositoryFragment.kt
+++ prasentation/src/main/java/com/raj/jadon/prasentation/trendingReposiotry/TreadingRepositoryFragment.kt
@@ -1,20 +1,21 @@
 package com.raj.jadon.prasentation.trendingReposiotry
 
+import androidx.fragment.app.viewModels
 import com.raj.jadon.prasentation.R
 import com.raj.jadon.prasentation.common.base.BaseFragment
 import com.raj.jadon.prasentation.databinding.FragmentTrendingRepositoryBinding
 import com.raj.jadon.prasentation.trendingReposiotry.viewModel.TreadingRepositoryViewModel
-import javax.inject.Inject
 
 class TreadingRepositoryFragment :
     BaseFragment<FragmentTrendingRepositoryBinding>(R.layout.fragment_trending_repository) {
 
-    @Inject
-    lateinit var trendingViewModel: TreadingRepositoryViewModel
+    private val trendingViewModel by viewModels<TreadingRepositoryViewModel>()
 
     override fun setDataCollector() {}
 
-    override fun setUpBindingVariables() {}
+    override fun setUpBindingVariables() {
+        trendingViewModel.getFun()
+    }
 
     override fun setClickListener() {}
 }
--- prasentation/src/main/java/com/raj/jadon/prasentation/trendingReposiotry/viewModel/TreadingRepositoryViewModel.kt
+++ prasentation/src/main/java/com/raj/jadon/prasentation/trendingReposiotry/viewModel/TreadingRepositoryViewModel.kt
@@ -3,9 +3,15 @@ package com.raj.jadon.prasentation.trendingReposiotry.viewModel
 import androidx.lifecycle.ViewModel
 import com.raj.jadon.domain.trandingRepositori.useCase.GetTrendingRepoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class TreadingRepositoryViewModel @Inject constructor(
     private val getTrendingRepoUseCase: GetTrendingRepoUseCase
-) : ViewModel()
+) : ViewModel() {
+
+    fun getFun() {
+        Timber.e("Hi............")
+    }
+}
